931c97053a7ba4292d774ce5c888ebbe380661bb
Kemal Kocabıyık <kemal@vngrs.com>
2016-04-13 00:47:47 +0300

    Initial Commit

---
+++ VersionCheckerExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VersionCheckerExample
+//
+//  Created by Kemal Kocabiyik on 13/04/16.
+//  Copyright © 2016 Ovidos Creative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VersionCheckerExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VersionCheckerExample
+//
+//  Created by Kemal Kocabiyik on 13/04/16.
+//  Copyright © 2016 Ovidos Creative. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
